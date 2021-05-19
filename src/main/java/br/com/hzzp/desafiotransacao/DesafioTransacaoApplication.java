0a4234433bc7b6c235b103f497e55007c5721289
Hzc <humberto.costa@zup.com.br>
2021-05-19 10:58:31 -0300

    Setup projeto

---
+++ src/main/java/br/com/hzzp/desafiotransacao/DesafioTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.hzzp.desafiotransacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTransacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hzzp/desafiotransacao/DesafioTransacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hzzp.desafiotransacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTransacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
